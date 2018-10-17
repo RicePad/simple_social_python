3e684839aea904806238b4f589fcf55c56185095
JON <jonn.thinkrtc@gmail.com>
2018-10-17 12:21:08 -0400

    updated grop admin and added group urls

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from groups.models import Group, GroupMember
 
-# Register your models here.
+# Register your GROUP models here.
+
+class GroupMemberInLine(admin.TabularInline):
+    model = GroupMember
+
+admin.site.register(Group)
